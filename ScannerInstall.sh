91a0f7e5d6ea4366dde065c19b870a635d6635fa
XurrencyFX <ultima2xd@gmail.com>
2023-08-24 10:25:16 -0400

    Update ScannerInstall.sh
    Translation

--- ScannerInstall.sh
+++ ScannerInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\e[1;32mInstalando dependencias"
+echo -e "\e[1;32mInstalling dependencies"
 yum install cpan wget curl glibc.i686 -y
 cpan force install Parallel::ForkManager
 cpan force install IO::Socket
@@ -9,4 +9,4 @@ yum install gcc php-devel php-pear libssh2 libssh2-devel libpcap -y
 pecl install -f ssh2
 touch /etc/php.d/ssh2.ini
 echo extension=ssh2.so > /etc/php.d/ssh2.ini
-echo -e "\e[1;36mTodo listo para sshbrute"
+echo -e "\e[1;36mAll set for ssh brute"
