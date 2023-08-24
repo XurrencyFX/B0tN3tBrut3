9a12ad207886bf1778f0ac5f2dfeaa77f3d0b7e2
XurrencyFX <ultima2xd@gmail.com>
2023-08-24 10:26:37 -0400

    Update Debian|Ubuntu_zmap_auto_install.sh

--- Debian|Ubuntu_zmap_auto_install.sh
+++ Debian|Ubuntu_zmap_auto_install.sh
@@ -2,8 +2,8 @@
 
 # Debian ZMap Install
 
-apt-get update -y
+sudo apt-get update -y
 
-apt-get install build-essential cmake libgmp3-dev gengetopt libpcap-dev flex byacc libjson-c-dev pkg-config libunistring-dev -y
+sudo apt-get install build-essential cmake libgmp3-dev gengetopt libpcap-dev flex byacc libjson-c-dev pkg-config libunistring-dev -y
 
-apt-get install zmap -y
+sudo apt-get install zmap -y
